bb5c100d08cd81e160d1de5c51bb9e04b6c27730
Victoria Bench <1611349@students.wits.ac.za>
2019-05-16 10:33:25 +0200

    Sample data and commands.sh commit

---
+++ commands.sh
@@ -0,0 +1,6 @@
+Commands Required:
+
+1) wget http://dept.ee.wits.ac.za/~scott/ELEN3020/lectlist.csv
+2) wget http://dept.ee.wits.ac.za/~scott/ELEN3020/examlist.csv
+
+(I performed these commands while in the /ltB/test/ directory)
